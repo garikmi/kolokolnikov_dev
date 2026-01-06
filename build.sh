8535a0b4281a397fb73dc8dc5cc16e8a6653f5c1
garikmi <imgarik@icloud.com>
2026-01-05 18:50:14 -0800

    Build script and improvements.

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+compile_css() {
+    tailwindcss -i ./themes/kolokolnikov-theme/assets/css/input.css -o ./themes/kolokolnikov-theme/assets/css/main.css "$@"
+}
+
+hugo_serve() {
+    hugo server "$@" --disableFastRender
+}
+
+python_server() {
+    python3 -m http.server 8080
+}
+
+if [ "$#" -lt 1 ]; then
+    hugo_serve "-D"
+else
+    if [ "$1" = "serve" ]; then
+        hugo_serve
+    elif [ "$1" = "drafts" ]; then
+        hugo_serve "-D"
+    elif [ "$1" = "server" ]; then
+        python_server
+    elif [ "$1" = "css" ]; then
+        compile_css
+    elif [ "$1" = "css-watch" ]; then
+        compile_css "--watch"
+    else
+        echo "Unknown command . . ."
+    fi
+fi
